4141370ebbb86d37c78f06dd4a0de8deee0d0c2f
Mohammed Khaled <140964017+MoKhaled1112@users.noreply.github.com>
2023-08-03 12:09:13 +0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,14 +16,14 @@ const navigation = [
       { title: 'Work Cases', href: '/work' },
     ],
   },
-  {
-    title: 'Work',
-    links: [
-      { title: 'FamilyFund', href: '/work/family-fund' },
-      { title: 'Unseal', href: '/work/unseal' },
-      { title: 'Phobia', href: '/work/phobia' },
-    ],
-  },
+    //   t{
+  //   title: 'Work',
+  //   links: [
+  //     { title: 'FamilyFund', href: '/work/family-fund' },
+  //     { title: 'Unseal', href: '/work/unseal' },
+  //     { title: 'Phobia', href: '/work/phobia' },
+  //   ],
+  // },
   {
     title: 'Connect',
     links: socialMediaProfiles,
