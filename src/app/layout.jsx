5964c6760b0346391c118bf1926791f46cc2e8d0
Mohammed Khaled <48189737+MoKhaled112@users.noreply.github.com>
2023-08-11 11:28:23 +0300

    Temporarily remove the two facebook plugins.
    The working versions for both plugins were:
    
    Facebook: 1c6b147cd0db75c85491044ba531920e32c6fa3d
    
    Manychat:
    https://github.com/MoKhaled112/ecomsAI/commit/e5b7cd3595ad0ca5ac0f192f4931b8459bded070

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,5 @@
 import { RootLayout } from '@/components/RootLayout'
 import '@/styles/tailwind.css'
-import Head from 'next/head';
 
 export const metadata = {
   title: {
@@ -12,36 +11,10 @@ export const metadata = {
 export default function Layout({ children }) {
   return (
     <html id="top" lang="en" className="h-full bg-background text-secondary text-base antialiased">
-      <head>
-        {/* Include the Facebook Messenger Chat Plugin code here */}
-        <script dangerouslySetInnerHTML={{
-          __html: `
-            window.fbAsyncInit = function() {
-              FB.init({
-                xfbml: true,
-                version: 'v17.0'
-              });
-            };
-
-            (function(d, s, id) {
-              var js, fjs = d.getElementsByTagName(s)[0];
-              if (d.getElementById(id)) return;
-              js = d.createElement(s); js.id = id;
-              js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-              fjs.parentNode.insertBefore(js, fjs);
-            }(document, 'script', 'facebook-jssdk'));
-          `
-        }} />
-
-        
-      </head>
 
       
       <body className="flex min-h-full flex-col">
         <RootLayout>{children}</RootLayout>
-        <div id="fb-root"></div>
-        <div id="fb-customer-chat" className="fb-customerchat" page_id="747804045285782" attribution="biz_inbox"></div>
-
         <a href="#top" className="back-to-top-button" aria-label="Back to Top">
           <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 11l7-7 7 7M5 19l7-7 7 7" />
