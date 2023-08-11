a77bc8b56d50a2f6855c5ac52f0f01ceef4262aa
Mohammed Khaled <48189737+MoKhaled112@users.noreply.github.com>
2023-08-11 10:53:38 +0300

    Added Facebook Messenger Chat Plugin

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -15,9 +15,35 @@ export default function Layout({ children }) {
       <head>
         <script src="//widget.manychat.com/747804045285782.js" defer="defer"></script>
         <script src="https://mccdn.me/assets/js/widget.js" defer="defer"></script>
+        {/* Include the Facebook Messenger Chat Plugin code here */}
+        <div id="fb-root"></div>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            window.fbAsyncInit = function() {
+              FB.init({
+                xfbml: true,
+                version: 'v17.0'
+              });
+            };
+
+            (function(d, s, id) {
+              var js, fjs = d.getElementsByTagName(s)[0];
+              if (d.getElementById(id)) return;
+              js = d.createElement(s); js.id = id;
+              js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+              fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));
+          `
+        }} />
+
+        
       </head>
+
+      
       <body className="flex min-h-full flex-col">
         <RootLayout>{children}</RootLayout>
+        <div id="fb-customer-chat" className="fb-customerchat" page_id="747804045285782" attribution="biz_inbox"></div>
+
         <a href="#top" className="back-to-top-button" aria-label="Back to Top">
           <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 11l7-7 7 7M5 19l7-7 7 7" />
